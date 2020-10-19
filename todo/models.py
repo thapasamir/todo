1d450aceed26a7cfbcde43d46e314a9e027b934b
Yaman-84 <63846784+Yaman-84@users.noreply.github.com>
2020-10-19 19:38:19 +0545

    Update models.py
    increasing max_length

--- todo/models.py
+++ todo/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class task(models.Model):
-    work = models.CharField(max_length=10, null=True)
-    description = models.TextField(max_length=200, null=True)
+    work = models.CharField(max_length=100000, null=True)
+    description = models.TextField(max_length=2000000, null=True)
 
     def __str__(self):
         return self.work
